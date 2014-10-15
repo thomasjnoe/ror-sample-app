af3da7c804e1c79ae4d98e52a96807e265c9354e
Thomas Noe <thomasjnoe@gmail.com>
2014-10-15 00:09:03 -0400

    Rearranged static page titles per tutorial

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,7 +12,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
-    	expect(page).to have_title("#{base_title} | Home")
+    	expect(page).to have_title("Home | #{base_title}")
     end
   end
 
@@ -24,7 +24,7 @@ describe "Static pages" do
 
   	it "should have the title 'Help'" do
     	visit '/static_pages/help'
-    	expect(page).to have_title("#{base_title}  | Help")
+    	expect(page).to have_title("Help | #{base_title}")
     end
   end
 
@@ -36,7 +36,7 @@ describe "Static pages" do
 
   	it "should have the title 'About Us'" do
     	visit '/static_pages/about'
-    	expect(page).to have_title("#{base_title}  | About Us")
+    	expect(page).to have_title("About Us | #{base_title}")
     end
   end
 
@@ -45,5 +45,10 @@ describe "Static pages" do
   		visit '/static_pages/contact'
   		expect(page).to have_content('Contact')
   	end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Contact | #{base_title}")
+    end
   end
 end
